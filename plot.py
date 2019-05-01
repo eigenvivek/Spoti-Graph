1cc1ee108be926a96507567cb1a4ce54e02e56a9
v715 <vgopala4@jhu.edu>
2019-05-01 00:09:20 -0400

    simple plotting function that overlays artist name on node

---
+++ plot.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+def plot(G):
+
+    node_attrs = nx.get_node_attributes(G, 'name')
+    custom_node_attrs = {}
+    for node, attr in node_attrs.items():
+        custom_node_attrs[node] = attr
+
+    pos_nodes = nx.spring_layout(G, k=0.15, iterations=20)
+
+    nx.draw_networkx(G, pos_nodes, labels=custom_node_attrs)
+    plt.show()
