e4726c3ad6aff7cd92efb90f56fb34a25d132793
v715 <vgopala4@jhu.edu>
2019-05-12 02:09:07 -0400

    restructured all functions to use class structure

--- search.py
+++ search.py
@@ -1,174 +1,188 @@
-from pprint import pprint
-
 import pickle
-
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from credentials import CLIENT_ID, CLIENT_SECRET
 
 
-def setup_scrapper(client_id, client_secret):
-    """Return a authenticated Spotify object."""
-    client_credentials_manager = SpotifyClientCredentials(
-        client_id, client_secret)
-    spotify = spotipy.Spotify(
-        client_credentials_manager=client_credentials_manager)
+class NetworkMiner():
 
-    return spotify
+    def __init__(
+        self,
+        include_collaborators=True,
+        breadth_limit=5,
+        max_pop_size=100,
+        min_popularity=65,
+        verbose=True,
+    ):
+        # Object to interface with Spotify interface
+        self.scrapper = self._setup_scrapper()
 
-
-def _get_artist_metadata(uri, attributes, scrapper):
-    """Return well formatted metadata. Fields of interest are specified in `attributes`."""
-
-    # Search for the artist
-    artist = scrapper.artist(uri)
-
-    # Collect all valid metadata
-    artist_metadata = {}
-    for attr in attributes:
-        try:
-            artist_metadata[attr] = artist[attr]
-        except KeyError:
-            print("{} is not a valid key".format(attr))
-
-    return artist_metadata
+        # Search paremeters
+        self.include_collaborators=include_collaborators
+        self.breadth_limit=breadth_limit
+        self.max_pop_size=max_pop_size
+        self.min_popularity=min_popularity
+        self.verbose=verbose
 
 
-def _get_collaborators(uri, limit, attributes, scrapper):
-    """Find artists that the given artist has collaborated with."""
+    def _setup_scrapper(self):
+        """Initialize Spotify object with proper authentification."""
 
-    # Get the top tracks
-    top_tracks = scrapper.artist_top_tracks(uri)
-    top_tracks = top_tracks['tracks'][0:limit]
+        from credentials import CLIENT_ID, CLIENT_SECRET
 
-    # Get the metadata for each artist and store in list
-    collaborators = []
-    for track in top_tracks:
-        for artist in track['artists']:
-            target = artist['uri']
-            if (target == uri) or (artist in collaborators):
+        client_credentials_manager = SpotifyClientCredentials(CLIENT_ID, CLIENT_SECRET)
+        spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+        return spotify
+
+
+    def _get_artist_metadata(self, uri, attributes):
+        """Return well formatted metadata. Fields of interest are specified in `attributes`."""
+
+        # Search for the artist
+        artist = self.scrapper.artist(uri)
+
+        # Collect all valid metadata
+        artist_metadata = {}
+        for attr in attributes:
+            try:
+                artist_metadata[attr] = artist[attr]
+            except KeyError:
+                print("{} is not a valid key".format(attr))
+
+        return artist_metadata
+
+
+    def _get_collaborators(self, uri, attributes):
+        """Find artists that the given artist has collaborated with."""
+
+        # Get the top tracks
+        top_tracks = self.scrapper.artist_top_tracks(uri)
+        top_tracks = top_tracks['tracks'][0:self.breadth_limit]
+
+        # Get the metadata for each artist and store in list
+        collaborators = []
+        for track in top_tracks:
+            for artist in track['artists']:
+                target = artist['uri']
+                if (target == uri) or (artist in collaborators):
+                    continue
+
+                artist = self.scrapper.artist(target)
+                collaborators.append(artist)
+
+        return collaborators
+
+
+    def _get_related_artists(
+        self,
+        uri,
+        connections,
+        metadata,
+        attributes,
+    ):
+        """Recursive implementation of depth-first search."""
+
+        if len(metadata.keys()) == self.max_pop_size:
+            raise RuntimeError("Maximum population size reached.")
+
+        # Add input artist's attributes to metadata
+        metadata[uri] = self._get_artist_metadata(uri, attributes=attributes)
+        if self.verbose:
+            # "Node <NUMBER>: <ARTIST> (<POPULARITY>)"
+            msg = "Node {}: {} ({})".format(len(metadata.keys()),
+                                metadata[uri]['name'], metadata[uri]['popularity'])
+            print(msg)
+
+        # Get up to `self.breadth_limit` related artists and associated metadata, organized in a list
+        related = self.scrapper.artist_related_artists(uri)
+        related = related['artists'][0:self.breadth_limit]
+
+        # Get up to `self.breadth_limit` collaborators and add unique artists to `related`
+        if self.include_collaborators:
+            collaborators = self._get_collaborators(uri, attributes=attributes)
+            related_uri = [artist['uri'] for artist in related]
+            for artist in collaborators:
+                if artist['uri'] not in related_uri:
+                    related.append(artist)
+
+        # Iterate through related artists...
+        for artist in related:
+
+            # 1a. Skip unpopular artists
+            if int(artist['popularity']) < self.min_popularity:
+                if self.verbose:
+                    msg = "\t{} is unpopular ({})".format(artist['name'], artist['popularity'])
+                    print(msg)
                 continue
 
-            artist = scrapper.artist(target)
-            collaborators.append(artist)
-
-    return collaborators
-
-
-def _get_related_artists(uri, connections, metadata, attributes, scrapper, limit, min_popularity, verbose, include_collaborators, breakpoint):
-    """Recursive implementation of depth-first search."""
-
-    if breakpoint is not None:
-        if len(metadata.keys()) == breakpoint:
-            raise Exception("breakpoint reached")
-
-    # Add input artist's attributes to metadata
-    metadata[uri] = _get_artist_metadata(
-        uri, attributes=attributes, scrapper=scrapper)
-    if verbose:
-        # "Node <NUMBER>: <ARTIST> (<POPULARITY>)"
-        pprint("Node {}: {} ({})".format(
-            len(metadata.keys()), metadata[uri]['name'], metadata[uri]['popularity']))
-
-    # Get up to `limit` related artists and associated metadata, organized in a list
-    related = scrapper.artist_related_artists(uri)
-    related = related['artists'][0:limit]
-
-    # Get up to `limit` collaborators and add unique artists to `related`
-    if include_collaborators:
-        collaborators = _get_collaborators(uri, limit, attributes, scrapper)
-        related_uri = [artist['uri'] for artist in related]
-        for artist in collaborators:
-            if artist['uri'] not in related_uri:
-                related.append(artist)
-
-    # Iterate through related artists...
-    for artist in related:
-
-        # 1a. Skip unpopular artists
-        if int(artist['popularity']) < min_popularity:
-            if verbose:
-                print("    {} is unpopular ({})".format(
-                    artist['name'], artist['popularity']))
-            continue
-
-        # 1b. Skip artists already adjacent to the input artist
-        try:
-            neighbors = [adjacent_uri for adjacent_uri in connections[uri]]
-            if artist['uri'] in neighbors:
+            # 1b. Skip artists already adjacent to the input artist
+            if connections.get(artist['uri']):
                 continue
-        except:
-            pass
 
-        # 2. Write related artist and weight to connections
-        input_artist_uri = str(uri)         # URI of the input artist
-        related_artist_uri = artist['uri']  # URI of the related artist
+            # 2. Write related artist and weight to connections
+            input_artist_uri = str(uri)         # URI of the input artist
+            related_artist_uri = artist['uri']  # URI of the related artist
+            try:
+                connections[input_artist_uri] += [related_artist_uri]
+            except KeyError:
+                connections[input_artist_uri] = [related_artist_uri]
+
+            # 3. Save the related artist's attributes
+            metadata[related_artist_uri] = self._get_artist_metadata(
+                related_artist_uri, attributes=attributes)
+
+            # 4. Recursively call `_get_related_artists`
+            self._get_related_artists(related_artist_uri, connections, metadata, attributes=attributes)
+
+
+    def _to_edgelist(self, connections, fname):
+        """Write dictionary of connections to a NetworkX-compatible weighted edgelist."""
+
+        with open('{}/{}.edgelist'.format('derivatives', fname), 'a') as f:
+            for artist, values in connections.items():
+                for related_arist in values:
+                    f.write("{} {}\n".format(artist, related_arist))
+
+
+    def _to_pickle(self, metadata, fname):
+        """Pickle dictionary of metadata."""
+
+        with open('{}/{}.pkl'.format('derivatives', fname), 'wb') as f:
+            pickle.dump(metadata, f, pickle.HIGHEST_PROTOCOL)
+
+
+    def write_edgelist(
+        self,
+        artist,
+        attributes=['name', 'popularity'],
+        fname=None,
+    ):
+        """
+        Main function for constructing graph of related artists.
+        `artist` should be a Spotify URI.
+        """
+
+        # Initialize empty dictionaries for outputs
+        connections = {}
+        metadata = {}
+
+        # Get the input artist's name
+        if fname is None:
+            fname = self.scrapper.artist(uri)['name']
+
+        # Run the search
         try:
-            connections[input_artist_uri] += [related_artist_uri]
-        except KeyError:
-            connections[input_artist_uri] = [related_artist_uri]
+            self._get_related_artists(
+                artist,
+                connections,
+                metadata,
+                attributes=attributes,
+            )
+        except RuntimeError as error:
+            print(repr(error))
 
-        # 3. Save the related artist's attributes
-        metadata[related_artist_uri] = _get_artist_metadata(
-            related_artist_uri, scrapper=scrapper, attributes=attributes)
-
-        # 4. Recursively call `_get_related_artists`
-        _get_related_artists(related_artist_uri, connections, metadata, attributes,
-                             scrapper, limit, min_popularity, verbose, include_collaborators, breakpoint)
-
-
-def _to_edgelist(connections, fname):
-    """Write dictionary of connections to a NetworkX-compatible weighted edgelist."""
-
-    with open('{}/{}.edgelist'.format('derivatives', fname), 'a') as f:
-        for artist, values in connections.items():
-            for related_arist in values:
-                f.write("{} {}\n".format(artist, related_arist))
-
-
-def _to_pickle(metadata, fname):
-    """Pickle dictionary of metadata."""
-
-    with open('{}/{}.pkl'.format('derivatives', fname), 'wb') as f:
-        pickle.dump(metadata, f, pickle.HIGHEST_PROTOCOL)
-
-
-def write_edgelist(artist, include_collaborators=True, limit=5, min_popularity=65, verbose=False, fname=None, breakpoint=None):
-    """
-    Main function for constructing graph of related artists.
-    `artist` should be a Spotify URI.
-    """
-
-    # Initialize Spotify object with proper authentification
-    spotify = setup_scrapper(CLIENT_ID, CLIENT_SECRET)
-
-    # Initialize empty dictionaries for outputs
-    connections = {}
-    metadata = {}
-
-    # Get the input artist's name
-    if fname is None:
-        artist = scrapper.artist(uri)
-        fname = artist['name']
-
-    # Run the search
-    try:
-        _get_related_artists(artist,
-                             connections,
-                             metadata,
-                             attributes=['name', 'popularity'],
-                             scrapper=spotify,
-                             limit=limit,
-                             min_popularity=min_popularity,
-                             include_collaborators=include_collaborators,
-                             breakpoint=breakpoint,
-                             verbose=verbose)
-
-    except Exception as error:
-        print(repr(error))
-
-    # Save the outputs
-    print("Saving edgelist...")
-    _to_edgelist(connections, fname='{}'.format(fname))
-    print("Saving attributes...")
-    _to_pickle(metadata, fname='{}_attributes'.format(fname))
+        # Save the outputs
+        print("Saving edgelist...")
+        self._to_edgelist(connections, fname='{}'.format(fname))
+        print("Saving attributes...")
+        self._to_pickle(metadata, fname='{}_attributes'.format(fname))
